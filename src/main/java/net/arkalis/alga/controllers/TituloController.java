4eb5cae08479d9110d8d5889f892ecb2c373a27c
Lianker Lopes <Lianker Lopes>
2016-02-09 21:25:38 -0200

    adição do combobox dinamicamente

--- src/main/java/net/arkalis/alga/controllers/TituloController.java
+++ src/main/java/net/arkalis/alga/controllers/TituloController.java
@@ -1,34 +1,43 @@
 package net.arkalis.alga.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.arkalis.alga.models.StatusTitulo;
 import net.arkalis.alga.models.Titulo;
 import net.arkalis.alga.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView modelAndView = new ModelAndView("CadastroTitulo");
+		return modelAndView;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView salvar(Titulo titulo) {
-		
 		titulos.save(titulo);
-		
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Titulo salvo com sucesso no banco de dados!");
-		return mv;
+		ModelAndView modelAndView = new ModelAndView("CadastroTitulo");
+		return modelAndView;
+	}
+
+	@ModelAttribute("allStatusTitulo")
+	public List<StatusTitulo> allStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 
 }
